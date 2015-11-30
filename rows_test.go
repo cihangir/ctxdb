8bd0a80a3c29e0b1b296c343a9316c6d93194950
Cihangir SAVAS <cihangir@koding.com>
2015-11-29 21:28:30 -0800

    Rows: added testing for rows/scan/next/err/close

--- rows_test.go
+++ rows_test.go
@@ -71,3 +71,151 @@ func TestRowsClose(t *testing.T) {
 		t.Fatalf("expected nil, got: %s", err)
 	}
 }
+
+func TestRowsColumns(t *testing.T) {
+	db := getConn(t)
+	ensureNullableTable(t, db)
+	ctx := context.Background()
+
+	if _, err := db.Exec(ctx, insertSqlStatement, 42, nil, 12); err != nil {
+		t.Fatalf("err while adding null item: %s", err.Error())
+	}
+
+	rows, err := db.Query(ctx, "SELECT string_n_val FROM nullable")
+	if err != nil {
+		t.Fatalf("expected nil, got: %s", err)
+	}
+
+	columns, err := rows.Columns(ctx)
+	if err != nil {
+		t.Fatalf("expected nil, got: %s", err)
+	}
+
+	if len(columns) != 1 {
+		t.Fatalf("expected 1 column, got: %d", len(columns))
+	}
+
+	if columns[0] != "string_n_val" {
+		t.Fatalf("expected string_n_val column, got: %d", columns[0])
+	}
+}
+
+func TestRowsColumnsWithTimeout(t *testing.T) {
+	db := getConn(t)
+	ensureNullableTable(t, db)
+	ctx := context.Background()
+
+	if _, err := db.Exec(ctx, insertSqlStatement, 42, nil, 12); err != nil {
+		t.Fatalf("err while adding null item: %s", err.Error())
+	}
+
+	timeout := time.Millisecond * 50
+	ctx, cancel := context.WithTimeout(ctx, timeout)
+	defer cancel()
+	rows, err := db.Query(ctx, "SELECT string_n_val FROM nullable")
+	if err != nil {
+		t.Fatalf("expected nil, got: %s", err)
+	}
+
+	time.Sleep(timeout * 2)
+
+	columns, err := rows.Columns(ctx)
+	if err != context.DeadlineExceeded {
+		t.Fatalf("expected context.DeadlineExceeded, got: %s", err)
+	}
+
+	if len(columns) != 0 {
+		t.Fatalf("expected 0 column, got: %d", len(columns))
+	}
+}
+
+func TestRowsScanNextErr(t *testing.T) {
+	db := getConn(t)
+	ensureNullableTable(t, db)
+	ctx := context.Background()
+
+	// prepare data set
+	for i := 1; i < 5; i++ {
+		if _, err := db.Exec(ctx, insertSqlStatement, i, nil, 42); err != nil {
+			t.Fatalf("err while adding null item: %s", err.Error())
+		}
+	}
+
+	rows, err := db.Query(ctx, "SELECT string_n_val FROM nullable WHERE int64_n_val > $1 ORDER BY int64_n_val", 0)
+	if err != nil {
+		t.Fatalf("expected nil, got: %s", err)
+	}
+
+	var i int64 = 1
+	for rows.Next(ctx) {
+		var int64_n_val int64
+		if err := rows.Scan(ctx, &int64_n_val); err != nil {
+			t.Fatalf("expected nil, got: %s", err)
+		}
+
+		if int64_n_val != i {
+			t.Fatalf("expected int64_n_val to be same with %d, got: %d", i, int64_n_val)
+		}
+
+		i++
+	}
+
+	if err := rows.Err(); err != nil {
+		t.Fatalf("expected nil, got: %s", err)
+	}
+
+	if err := rows.Close(ctx); err != nil {
+		t.Fatalf("expected nil, got: %s", err)
+	}
+}
+
+func TestRowsScanWithNoResult(t *testing.T) {
+	db := getConn(t)
+	ensureNullableTable(t, db)
+	ctx := context.Background()
+
+	rows, err := db.Query(ctx, "SELECT string_n_val FROM nullable WHERE int64_n_val <> int64_n_val")
+	if err != nil {
+		t.Fatalf("expected nil, got: %s", err)
+	}
+
+	for rows.Next(ctx) {
+		t.Fatalf("expected no result, but got")
+	}
+
+	if err := rows.Err(); err != nil {
+		t.Fatalf("expected nil, got: %s", err)
+	}
+
+	if err := rows.Close(ctx); err != nil {
+		t.Fatalf("expected nil, got: %s", err)
+	}
+}
+
+func TestRowsNextWithTimeout(t *testing.T) {
+	db := getConn(t)
+	ensureNullableTable(t, db)
+	ctx := context.Background()
+
+	rows, err := db.Query(ctx, "SELECT string_n_val FROM nullable WHERE int64_n_val > $1 ORDER BY int64_n_val", 0)
+	if err != nil {
+		t.Fatalf("expected nil, got: %s", err)
+	}
+
+	timeout := time.Millisecond * 50
+	ctx, cancel := context.WithTimeout(ctx, timeout)
+	defer cancel()
+
+	time.Sleep(timeout * 2)
+	for rows.Next(ctx) {
+		t.Fatalf("expected no result, but got")
+	}
+
+	if err := rows.Err(); err != context.DeadlineExceeded {
+		t.Fatalf("expected context.DeadlineExceeded, got: %s", err)
+	}
+
+	if err := rows.Close(ctx); err != context.DeadlineExceeded {
+		t.Fatalf("expected context.DeadlineExceeded, got: %s", err)
+	}
+}
